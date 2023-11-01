03742bf37ec81775f75676dc89cf156d300ab5fe
Aman Chaudhary <118996679+ac0910@users.noreply.github.com>
2023-11-01 18:34:27 +0530

    Add files via upload

--- break.c
+++ break.c
@@ -3,13 +3,17 @@ int main(){
     int n;
     printf("Enter the Value in N :");
     scanf("%d",&n);
+    int a=0;
     for (int i = 2; i<=n-1 ; i++)
     {
         if (n%i==0){
-            printf("The Given Number is composite");
+           a=1;
+            break;
         }
-       
+        
    }
+   if(a==0)  printf("The Given Number is Prime \n");
+   else  printf("The Given Number is composite");
    return 0;
     
 }
