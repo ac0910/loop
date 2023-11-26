50725bc1a8ba013c40776693cf2eec7f163ffa29
Aman Chaudhary <118996679+ac0910@users.noreply.github.com>
2023-11-26 20:22:00 +0530

    Add files via upload

--- solidrectangle.c
+++ solidrectangle.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 int main(){
     int n;
-    printf("Enter a  number :");
+    printf("Enter number of rows :");
     scanf("%d",&n);
+    int m;
+    printf("Enter number of columns :");
+    scanf("%d",&m);
     for (int i = 1; i <=n; i++)
     {
-        for (int i = 1; i <=n; i++)
+        for (int i = 1; i <=m; i++)
         {
         printf("*");
         }
